ec9677b1b1993e4ffa2058f54b36d15e26499ad9
SoudersT <tysouders32@gmail.com>
2024-11-22 11:17:56 -0500

    Database work

--- setup.sql
+++ setup.sql
@@ -3,3 +3,17 @@ CREATE TABLE users(
    uname           TEXT UNIQUE,
    password          TEXT
 );
+
+CREATE TABLE products(
+   productID      INT PRIMARY KEY,
+   Pname          TEXT,
+   category       TEXT,
+   price          INT
+);
+
+CREATE TABLE review(
+   reviewID       INT AUTOINCREMENT,
+   userID         TEXT,
+   productID      INT,
+   rating         INT,
+);
